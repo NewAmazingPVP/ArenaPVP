4297d454b87cce74a6c6de7a87e6720494351c7f
NewAmazingPVP <gamingofpat@gmail.com>
2023-11-05 16:24:45 -0500

    first push

---
+++ src/main/java/newamazingpvp/arenapvp/ArenaPVP.java
@@ -0,0 +1,17 @@
+package newamazingpvp.arenapvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArenaPVP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
