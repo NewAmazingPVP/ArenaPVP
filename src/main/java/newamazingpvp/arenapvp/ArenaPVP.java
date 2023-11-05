b3f8bd8ba7ac2e4491b03ba4fc6162ac067e426f
NewAmazingPVP <gamingofpat@gmail.com>
2023-11-05 16:33:25 -0500

    small push

--- src/main/java/newamazingpvp/arenapvp/ArenaPVP.java
+++ src/main/java/newamazingpvp/arenapvp/ArenaPVP.java
@@ -1,13 +1,18 @@
 package newamazingpvp.arenapvp;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ArenaPVP extends JavaPlugin {
 
+    public static ArenaPVP arenaPVP;
+    private FileConfiguration config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        config = getConfig();
+        arenaPVP = this;
     }
 
     @Override
