ac7796d954bdbb46a1e095e268e30422f2e3fac4
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-06 22:00:47 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/GeberalListeners/KillTest1.java
+++ src/main/java/newamazingpvp/arenapvp/GeberalListeners/KillTest1.java
@@ -1,6 +1,8 @@
 package newamazingpvp.arenapvp.GeberalListeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,11 +12,16 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class KillTest1 implements Listener {
 
+    int streak;
+
     @EventHandler
     public void onPlayerInteract(EntityDeathEvent e) {
         Player killer = e.getEntity().getKiller();
-        killer.sendMessage(ChatColor.BOLD + "" + ChatColor.GREEN + "KILL!" + ChatColor.AQUA + " +12XP");
+        streak +=1;
+        killer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "KILL!" + ChatColor.AQUA + " +12XP" + ChatColor.GOLD + " " + streak);
         killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
         killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
+        Location loc = e.getEntity().getLocation();
+        e.getEntity().getWorld().spawnParticle(Particle.SMOKE_NORMAL, loc, 10);
     }
 }
