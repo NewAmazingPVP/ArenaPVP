266f80f54373d98156aeafecf130d74733b93ca0
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-07 19:33:31 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/General_Listeners/Death_Event.java
+++ src/main/java/newamazingpvp/arenapvp/General_Listeners/Death_Event.java
@@ -1,5 +1,6 @@
 package newamazingpvp.arenapvp.General_Listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -13,7 +14,9 @@ public class Death_Event implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e) {
         Player player = e.getPlayer();
-
+        for (Player playeronline : Bukkit.getOnlinePlayers()) {
+            playeronline.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_RED + player + ChatColor.RED + " Has Died!");
+        }
         //cansel the death (in a way that will not break the game and your client)
         e.setDeathMessage(null);
         e.setDroppedExp(0);
@@ -21,13 +24,11 @@ public class Death_Event implements Listener {
 
         //death visuals
         player.playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 2.0f, 2.0f);
-        player.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "You Died", "", 10, 70, 20);
+        player.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "You Died", "", 0, 70, 20);
 
 
         //possible respawn locations
-        Location loc1 = new Location(e.getEntity().getWorld(), 19, 110, 59);
-        Location loc2 = new Location(e.getEntity().getWorld(), 22, 110, 61);
-        Location loc3 = new Location(e.getEntity().getWorld(), 27, 110, 61);
+        Location loc1 = new Location(e.getEntity().getWorld(), 25, 111, 62);
 
         e.getEntity().teleport(loc1);
     }
