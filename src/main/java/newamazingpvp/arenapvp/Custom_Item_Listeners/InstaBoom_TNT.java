9622d3a8302f7e1889f21a187e4ed491d39d6e9c
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-08 17:14:28 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
+++ src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
@@ -1,6 +1,6 @@
 package newamazingpvp.arenapvp.Custom_Item_Listeners;
 
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +8,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.List;
 
 public class InstaBoom_TNT implements Listener {
 
@@ -17,13 +19,22 @@ public class InstaBoom_TNT implements Listener {
         Player player = e.getPlayer();
 
         if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            if (e.getMaterial() == Material.getMaterial("111111111")) {
+            if (e.getMaterial() == Material.getMaterial(ChatColor.RED + "" + ChatColor.BOLD + "InstaBoom TNT")) {
                 e.setCancelled(true);
 
+                for (Player onlineplayer : Bukkit.getOnlinePlayers()) {
+                    onlineplayer.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.0f);
 
-
+                }
 
             }
+        }
+    }
+
+    public void onPlayerPlaceInstaboomTNT(BlockPlaceEvent e) {
+        Location location = e.getBlock().getLocation();
+        if (e.getBlock().getType() == Material.getMaterial(ChatColor.RED + "" + ChatColor.BOLD + "InstaBoom TNT")) {
+            location.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, location, 10);
         }
     }
 }
