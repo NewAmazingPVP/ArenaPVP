2c92991f6cc5ad782b9153916a433ead21d10ef4
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-08 20:21:37 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
+++ src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
@@ -8,37 +8,22 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
 public class InstaBoom_TNT implements Listener {
 
     @EventHandler
-    /*public void onPlayerPlaceInstaboomTNT(PlayerInteractEvent e) {
-
-        Player player = e.getPlayer();
-
-
-        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            if (e.getMaterial() == Material.getMaterial(ChatColor.RED + "" + ChatColor.BOLD + "InstaBoom TNT")) {
-                e.setCancelled(true);
-
-                for (Player onlineplayer : Bukkit.getOnlinePlayers()) {
-                    onlineplayer.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.0f);
-
-                }
-            }
-        }
-    }*/
-
-
     public void onPlayerPlaceInstaboomTNT(BlockPlaceEvent e) {
 
         Player player = e.getPlayer();
-
+        ItemStack item = e.getItemInHand();
+        ItemMeta meta = item.getItemMeta();
 
         Location location = e.getBlock().getLocation();
-        if (e.getBlock().getType() == Material.getMaterial(ChatColor.RED + "" + ChatColor.BOLD + "InstaBoom TNT")) {
+        if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.RED + "" + ChatColor.BOLD + "InstaBoom TNT")) {
             location.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, location, 10);
             e.setCancelled(true);
             for (Player onlineplayer : Bukkit.getOnlinePlayers()) {
--- src/main/java/newamazingpvp/arenapvp/General_Listeners/PlayerCount.java
+++ src/main/java/newamazingpvp/arenapvp/General_Listeners/PlayerCount.java
@@ -1,6 +1,7 @@
 package newamazingpvp.arenapvp.General_Listeners;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,26 +9,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-
 public class PlayerCount implements Listener {
 
-    public static int onlineplayers;
 
+    public static int onlineplayers;
     @EventHandler
+
+
     public void onplayerjoin(PlayerJoinEvent e) {
 
         e.setJoinMessage(null);
         onlineplayers += 1;
 
-
     }
+
 
     @EventHandler
     public void onplayerleave(PlayerQuitEvent e) {
 
         onlineplayers -=1;
-
     }
 }
 
