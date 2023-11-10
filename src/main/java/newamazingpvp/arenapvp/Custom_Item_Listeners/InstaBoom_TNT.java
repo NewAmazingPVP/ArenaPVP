c62287eaa07593cfdd2a1971e6a42cb220d724e3
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-09 19:34:30 -0500

    test

--- src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
+++ src/main/java/newamazingpvp/arenapvp/Custom_Item_Listeners/InstaBoom_TNT.java
@@ -31,14 +31,10 @@ public class InstaBoom_TNT implements Listener {
             e.setCancelled(true);
             for (Player onlineplayer : Bukkit.getOnlinePlayers()) {
                 onlineplayer.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.0f);
-                List<Player> nearbyPlayers = (List<Player>) location.getWorld().getNearbyPlayers(location, 1);
+                List<Player> nearbyPlayers = (List<Player>) location.getWorld().getNearbyPlayers(location, 20);
                 for (Player playernear : nearbyPlayers) {
-                    playernear.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 1, 20));
-
-                    Vector direction = playernear.getLocation().subtract(location).toVector();
-                    direction.normalize();
-                    // Push player away from the block
-                    playernear.teleport(playernear.getLocation().add(direction));
+                    playernear.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 1, 4));
+                    playernear.damage(3);
 
                     }
                 }
