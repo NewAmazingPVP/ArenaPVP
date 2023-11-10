869bd0e34906336eee40680f46c49c4a80891b39
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-09 20:32:15 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/General_Listeners/Mystic_Well.java
+++ src/main/java/newamazingpvp/arenapvp/General_Listeners/Mystic_Well.java
@@ -1,9 +1,6 @@
 package newamazingpvp.arenapvp.General_Listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
@@ -13,6 +10,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +25,10 @@ public class Mystic_Well implements Listener {
             if (block.getType() == Material.ENCHANTING_TABLE) {
                 event.setCancelled(true);
                 player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 2.0f, 0.0f);
+                Inventory well = Bukkit.createInventory(player, 54, ChatColor.RED + "" + ChatColor.BOLD + "" + ChatColor.MAGIC + "L" + ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + " Mystic Well " + ChatColor.RED + "" + ChatColor.BOLD + "" + ChatColor.MAGIC + "L");
+
+                player.openInventory(well);
+
             }
         }
     }
@@ -46,7 +48,7 @@ public class Mystic_Well implements Listener {
             armorStand.setMarker(true);
             armorStandMap.put(event.getBlock().getLocation().hashCode(), armorStand);
 
-            Location location2 = event.getBlock().getLocation().add(0.5, 1.1, 0.5);
+            /*Location location2 = event.getBlock().getLocation().add(0.5, 1.1, 0.5);
             ArmorStand armorStand2 = event.getBlock().getWorld().spawn(location2, ArmorStand.class);
             armorStand2.setCustomName(ChatColor.GRAY + "Item Enchants");
             armorStand2.setCustomNameVisible(true);
@@ -54,7 +56,7 @@ public class Mystic_Well implements Listener {
             armorStand2.setGravity(false);
             armorStand2.setInvulnerable(true);
             armorStand2.setMarker(true);
-            armorStandMap.put(event.getBlock().getLocation().hashCode(), armorStand2);
+            armorStandMap.put(event.getBlock().getLocation().hashCode(), armorStand2);*/
         }
     }
 
@@ -62,10 +64,10 @@ public class Mystic_Well implements Listener {
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.getBlock().getType() == Material.ENCHANTING_TABLE) {
             ArmorStand armorStand = armorStandMap.get(event.getBlock().getLocation().hashCode());
-            ArmorStand armorStand2 = armorStandMap.get(event.getBlock().getLocation().hashCode());
+            //ArmorStand armorStand2 = armorStandMap.get(event.getBlock().getLocation().hashCode());
             if (armorStand != null) {
                 armorStand.remove();
-                armorStand2.remove();
+                //armorStand2.remove();
                 armorStandMap.remove(event.getBlock().getLocation().hashCode());
             }
         }
