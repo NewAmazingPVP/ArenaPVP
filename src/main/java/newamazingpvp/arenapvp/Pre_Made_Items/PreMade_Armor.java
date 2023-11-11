88dc6dde80fe8abbf11fe0f8cbe7e053d3d5acaf
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-11 18:13:49 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/Pre_Made_Items/PreMade_Armor.java
+++ src/main/java/newamazingpvp/arenapvp/Pre_Made_Items/PreMade_Armor.java
@@ -14,15 +14,43 @@ public class PreMade_Armor {
 
     public static ItemStack startCP() {
 
-        ItemStack startCP = new ItemStack(Material.LEATHER_LEGGINGS);
-        LeatherArmorMeta leg = (LeatherArmorMeta) startCP.getItemMeta();
-        leg.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Starter Chestplate");
-        leg.setUnbreakable(true);
-        leg.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
-        leg.addItemFlags(ItemFlag.HIDE_DYE);
-        startCP.setItemMeta(leg);
+        ItemStack startCP = new ItemStack(Material.LEATHER_BOOTS);
+        LeatherArmorMeta startcpm = (LeatherArmorMeta) startCP.getItemMeta();
+        startcpm.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Starter Chestplate");
+        startcpm.setUnbreakable(true);
+        startcpm.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        startcpm.addItemFlags(ItemFlag.HIDE_DYE);
+        startCP.setItemMeta(startcpm);
 
 
         return startCP;
     }
+
+    public static ItemStack startLeg() {
+
+        ItemStack startLeg = new ItemStack(Material.LEATHER_LEGGINGS);
+        LeatherArmorMeta startlegm = (LeatherArmorMeta) startLeg.getItemMeta();
+        startlegm.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Starter Leggings");
+        startlegm.setUnbreakable(true);
+        startlegm.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        startlegm.addItemFlags(ItemFlag.HIDE_DYE);
+        startLeg.setItemMeta(startlegm);
+
+
+        return startLeg;
+    }
+
+    public static ItemStack startBoot() {
+
+        ItemStack startBoots = new ItemStack(Material.LEATHER_LEGGINGS);
+        LeatherArmorMeta startbootm = (LeatherArmorMeta) startBoots.getItemMeta();
+        startbootm.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "Starter Boots");
+        startbootm.setUnbreakable(true);
+        startbootm.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        startbootm.addItemFlags(ItemFlag.HIDE_DYE);
+        startBoots.setItemMeta(startbootm);
+
+        return startBoots;
+    }
+
 }
