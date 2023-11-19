5493dedf287d3b0d9d470f5bb84fc5f59202b30b
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-18 21:45:09 -0500

    1

--- src/main/java/newamazingpvp/arenapvp/Kill_Streak_Info/Kill_Listener.java
+++ src/main/java/newamazingpvp/arenapvp/Kill_Streak_Info/Kill_Listener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import static newamazingpvp.arenapvp.ArenaPVP.arenaPVP;
@@ -13,29 +14,38 @@ import static newamazingpvp.arenapvp.Pre_Made_Items.PreMadeItems_Custom.MysticBo
 
 public class Kill_Listener implements Listener {
 
+    private final HashMap<UUID, Integer> streaks = new HashMap<>();
 
     @EventHandler
     public void onPlayerInteract(EntityDeathEvent e) {
         Player killer = e.getEntity().getKiller();
         Location loc = e.getEntity().getLocation();
 
+        UUID uuid = killer.getUniqueId();
 
-        if (Math.random() < 0.5) {
+        if (streaks.containsKey(uuid)) {
+            streaks.put(uuid, streaks.get(uuid) + 1);
+        } else {
+            streaks.put(uuid, 1);
 
-            killer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "KILL!" + ChatColor.AQUA + " +12XP ⚔");
-            killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
+
+            if (Math.random() < 0.5) {
+
+                killer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "KILL!" + ChatColor.AQUA + " +12XP ⚔" + " " + streaks.get(uuid));
+                killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
 
 
-        } else if (Math.random() < 0.5) {
+            } else if (Math.random() > 0.5) {
 
-            killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
-            killer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "KILL!" + ChatColor.AQUA + " +12XP ⚔");
-            killer.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "RARE DROP!" + ChatColor.DARK_PURPLE + " +1 Mystic Box");
-            e.getEntity().getWorld().spawnParticle(Particle.WATER_WAKE, loc, 5);
-            killer.getInventory().addItem(MysticBox());
-            Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 0.0f), 1);
-            Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f), 11);
-            Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 3.0f), 21);
+                killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0f, 2.0f);
+                killer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "KILL!" + ChatColor.AQUA + " +12XP ⚔" + " " + streaks.get(uuid));
+                killer.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "RARE DROP!" + ChatColor.DARK_PURPLE + " +1 Mystic Box");
+                e.getEntity().getWorld().spawnParticle(Particle.WATER_WAKE, loc, 5);
+                killer.getInventory().addItem(MysticBox());
+                Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 0.0f), 1);
+                Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f), 11);
+                Bukkit.getScheduler().runTaskLater(arenaPVP, () -> killer.playSound(killer.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 3.0f), 21);
+            }
         }
     }
 }
